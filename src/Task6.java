3d1f0108a111528ce83ed01f7dd4b108246bfb1d
Nodrawtexture <102799514+Nodrawtexture@users.noreply.github.com>
2022-11-07 16:06:44 +0300

    Delete Task6.java

--- src/Task6.java
+++
@@ -1,9 +0,0 @@
-public class Task6 {
-    public static void main(String[] args){
-        System.out.println("Первые 10 чисел гармонического ряда:");
-        for (int n=1; n<=10; n++){
-            System.out.printf("%.3f", 1.0 / n);
-            System.out.println();
-        }
-    }
-}
